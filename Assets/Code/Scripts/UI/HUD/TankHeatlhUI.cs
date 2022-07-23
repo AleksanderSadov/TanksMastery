a9448409e98f70099fa7d3fc19b781dbc808c162
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-07-23 11:47:21 +0700

    Change health UI to player controls color

--- Assets/Code/Scripts/UI/HUD/TankHeatlhUI.cs
+++ Assets/Code/Scripts/UI/HUD/TankHeatlhUI.cs
@@ -9,14 +9,16 @@ namespace Tanks.UI
     {
         public Slider slider;
         public Image fillImage;
-        public Color fullHealthColor = Color.green;
-        public Color zeroHealthColor = Color.red;
+        public Color playerOneColor = Color.green;
+        public Color playerTwoColor = Color.magenta;
 
         private TankHealth health;
+        private TankPlayerController playerController;
 
         private void Start()
         {
             health = GetComponent<TankHealth>();
+            playerController = GetComponent<TankPlayerController>();
         }
 
         private void OnEnable()
@@ -26,18 +28,31 @@ namespace Tanks.UI
 
         private void LateUpdate()
         {
-            SetHealthUI();
+            slider.value = health.currentHealth;
+
+            if (playerController != null && playerController.enabled)
+            {
+                slider.gameObject.SetActive(true);
+
+                if (playerController.playerControlsNumber == PlayerControlsNumber.FIRST)
+                {
+                    fillImage.color = playerOneColor;
+                }
+
+                if (playerController.playerControlsNumber == PlayerControlsNumber.SECOND)
+                {
+                    fillImage.color = playerTwoColor;
+                }
+            }
+            else
+            {
+                slider.gameObject.SetActive(false);
+            }
         }
 
         private void OnDisable()
         {
             slider.gameObject.SetActive(false);
-        }
-
-        private void SetHealthUI()
-        {
-            slider.value = health.currentHealth;
-            fillImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, health.currentHealth / health.startingHealth);
         }
     }
 }
